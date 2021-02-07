14b340e1a3bd5483bd1d37c8bf925848bdf1f6e9
Mathieu JOLY <m.joly@live.fr>
2021-02-07 19:57:21 +0100

    add react

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import './App.scss';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+
+	return (
+	    <div className='App'>
+			<header>
+				<h1>Do Note React Again</h1>
+			</header>
+	    </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
